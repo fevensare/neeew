d61c33c629001802203c9d6ec440244b0d001ae6
fevensare <fevensare@gmail.com>
2024-01-24 00:01:33 +0300

    fdd

---
+++ Untitled3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{// We know that we need a constant number of elements
+const int max = 10;
+int number[max];// We will calculate their sum
+int sum = 0;
+cout << "Please type 10 integers.\n";
+for( int i = 0; i < max; i++ )
+{ 
+cout << "Number " << i + 1 << ": ";
+cin >> number[i];
+sum += number[i];
+}
+cout << "\n\nThe sum of these numbers is " << sum << "\n\n";
+return 0;
+}
+
+
